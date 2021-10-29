02f387d9da1efb540541bcced8ac23cac97cc892
neosgc <diegoaliagalahuerta@gmail.com>
2021-10-29 09:26:46 +0200

    Thats pretty much it

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,12 +28,16 @@ namespace ShutdownTimer
             
         }
 
+        //
+        int CustomSeconds=0;
+        int CustomMinutes=30;
+        int CustomHours=0;
+        //
 
-
-
-
-
-
+        private void btn_lockdown_Click(object sender, RoutedEventArgs e)
+        {
+            Lockdown();
+        }
 
         private void ShutdownIn(int seconds)
         {
@@ -70,8 +75,67 @@ namespace ShutdownTimer
             Console.WriteLine(cmd.StandardOutput.ReadToEnd());
         }
 
+        private void Lockdown()
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("rundll32.exe user32.dll,LockWorkStation");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        private void btn_cancelSD_Click(object sender, RoutedEventArgs e)
+        {
+            CancelShutdown();
+        }
+
+        private void btn_shutdown1h_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownIn(3600);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //custom
+        {
+            int CustomSecondsFinal = ((CustomHours * 3600) + (CustomMinutes * 60) + CustomSeconds);
+            ShutdownIn(CustomSecondsFinal);
+        }
+
+        private void tbx_hours_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_hours = (TextBox)sender;
+            string hoursString = tbx_hours.Text;
+            CustomHours = Int32.Parse(hoursString);
+        }
+
+        private void tbx_minutes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_minutes = (TextBox)sender;
+            string minutesString = tbx_minutes.Text;
+            CustomMinutes = Int32.Parse(minutesString);
+        }
+
+        private void tbx_seconds_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_seconds = (TextBox)sender;
+            string secondsString = tbx_seconds.Text;
+            CustomSeconds = Int32.Parse(secondsString);
+        }
 
 
 
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
     }
 }
