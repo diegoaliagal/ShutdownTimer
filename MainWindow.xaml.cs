000ba17176426aefc184342a0409858c03b6393b
neosgc <diegoaliagalahuerta@gmail.com>
2021-10-29 09:58:54 +0200

    preparing for release

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ShutdownTimer
 {
@@ -34,28 +24,88 @@ namespace ShutdownTimer
         int CustomHours=0;
         //
 
+        // presets
+        private void btn_cancelSD_Click(object sender, RoutedEventArgs e)
+        {
+            CancelShutdown();
+        }
         private void btn_lockdown_Click(object sender, RoutedEventArgs e)
         {
             Lockdown();
         }
+        private void btn_shutdown1h_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownIn(3600);
+        }
+        private void btn_shutdown2h_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownIn(7200);
+        }
+        private void Shutdown_in_5h_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownIn(18000);
+        }
+        private void btn_shutdown12h_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownIn(43200);
+        }
+
+
+        // ends presets
+        // custom stuff
+        private void Button_Click(object sender, RoutedEventArgs e) //custom
+        {
+            int CustomSecondsFinal = ((CustomHours * 3600) + (CustomMinutes * 60) + CustomSeconds);
+            ShutdownIn(CustomSecondsFinal);
+        }
+
+        private void tbx_hours_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_hours = (TextBox)sender;
+            string hoursString = tbx_hours.Text;
+            CustomHours = Int32.Parse(hoursString);
+        }
+
+        private void tbx_minutes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_minutes = (TextBox)sender;
+            string minutesString = tbx_minutes.Text;
+            CustomMinutes = Int32.Parse(minutesString);
+        }
+
+        private void tbx_seconds_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbx_seconds = (TextBox)sender;
+            string secondsString = tbx_seconds.Text;
+            CustomSeconds = Int32.Parse(secondsString);
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        // end custom stuff
+        // commands
 
         private void ShutdownIn(int seconds)
         {
 
-           Process cmd = new Process();
-           cmd.StartInfo.FileName = "cmd.exe";
-           cmd.StartInfo.RedirectStandardInput = true;
-           cmd.StartInfo.RedirectStandardOutput = true;
-           cmd.StartInfo.CreateNoWindow = true;
-           cmd.StartInfo.UseShellExecute = false;
-           cmd.Start();
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
 
-           cmd.StandardInput.WriteLine("shutdown -s -t "+ seconds);
-           cmd.StandardInput.Flush();
-           cmd.StandardInput.Close();
-           cmd.WaitForExit();
-           Console.WriteLine(cmd.StandardOutput.ReadToEnd());
-        
+            cmd.StandardInput.WriteLine("shutdown -s -t " + seconds);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
         }
 
         private void CancelShutdown()
@@ -92,50 +142,12 @@ namespace ShutdownTimer
             Console.WriteLine(cmd.StandardOutput.ReadToEnd());
         }
 
-        private void btn_cancelSD_Click(object sender, RoutedEventArgs e)
-        {
-            CancelShutdown();
-        }
-
-        private void btn_shutdown1h_Click(object sender, RoutedEventArgs e)
-        {
-            ShutdownIn(3600);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) //custom
-        {
-            int CustomSecondsFinal = ((CustomHours * 3600) + (CustomMinutes * 60) + CustomSeconds);
-            ShutdownIn(CustomSecondsFinal);
-        }
-
-        private void tbx_hours_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox tbx_hours = (TextBox)sender;
-            string hoursString = tbx_hours.Text;
-            CustomHours = Int32.Parse(hoursString);
-        }
-
-        private void tbx_minutes_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox tbx_minutes = (TextBox)sender;
-            string minutesString = tbx_minutes.Text;
-            CustomMinutes = Int32.Parse(minutesString);
-        }
-
-        private void tbx_seconds_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox tbx_seconds = (TextBox)sender;
-            string secondsString = tbx_seconds.Text;
-            CustomSeconds = Int32.Parse(secondsString);
-        }
+        
 
 
 
 
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
-        {
-            System.Text.RegularExpressions.Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+
+        //ends commands
     }
 }
